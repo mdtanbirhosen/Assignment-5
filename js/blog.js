4d2a873f4de7f37f89e96181d1b8d2fca95a55a7
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2024-09-23 19:43:23 +0600

    navbar responsived

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,28 @@
-document.getElementById('home-btn').addEventListener('click',function(){
+document.getElementById('home-btn').addEventListener('click', function () {
     window.location.href = './index.html'
 })
+// dropdown questions
+// question 1
+document.getElementById('question1')
+    .addEventListener('click', function () {
+        const answer = document.getElementById("answer1");
+            answer.classList.toggle("hidden");
+    });
+    // question 2
+document.getElementById('question2')
+.addEventListener('click', function () {
+    const answer = document.getElementById("answer2");
+        answer.classList.toggle("hidden");
+});
+// question 3
+document.getElementById('question3')
+    .addEventListener('click', function () {
+        const answer = document.getElementById("answer3");
+            answer.classList.toggle("hidden");
+    });
+    // question 4
+document.getElementById('question4')
+.addEventListener('click', function () {
+    const answer = document.getElementById("answer4");
+        answer.classList.toggle("hidden");
+});
